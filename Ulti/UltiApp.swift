ef22b6eb346cf737a039fba91c8c04c0a2086b42
Niuffi <kamirat952@student.polsl.pl>
2022-04-25 21:24:51 +0200

    Initial Commit

---
+++ Ulti/UltiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltiApp.swift
+//  Ulti
+//
+//  Created by Kamil on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct UltiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
